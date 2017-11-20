872321443ff42eb66fb0662f0b10c852a9c59fb3
Artem Belkov <bestk1ngarthur@aol.com>
2017-11-20 15:13:26 +0300

    Initial Commit

---
+++ NetworkHW/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NetworkHW
+//
+//  Created by a.belkov on 20/11/2017.
+//  Copyright © 2017 bestK1ng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NetworkHW/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NetworkHW
+//
+//  Created by a.belkov on 20/11/2017.
+//  Copyright © 2017 bestK1ng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
